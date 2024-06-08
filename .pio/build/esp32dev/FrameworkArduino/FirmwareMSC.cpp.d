8ac9a7a577bb9096551e40df9e50951932e59c08
pro-codes090 <procodes090@gmail.com>
2024-06-08 14:39:51 +0530

    added .pio

---
+++ .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
+ C:/Users/pratham/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ C:/Users/pratham/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ C:/Users/pratham/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/pratham/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ .pio/build/esp32dev/lib9d4/SD/sd_diskio_crc.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/lib9d4/SD/sd_diskio_crc.c.o: \
+ C:/Users/pratham/.platformio/packages/framework-arduinoespressif32/libraries/SD/src/sd_diskio_crc.c
